820e9b05ca792e89c2a83979622f16471ea0dc0d
sharvann0 <129393708+sharvann0@users.noreply.github.com>
2024-06-02 07:49:52 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "Sharvan"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => value not defined 
+// symbol => unique 
+// null => object
+
+// object 
+console.table([typeof(name),typeof(age),typeof(isLoggedIn),typeof(null),typeof(undefined)])
