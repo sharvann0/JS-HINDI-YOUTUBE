9b2725486309fb15b3fb6b2374cd8841617e40f2
sharvann0 <129393708+sharvann0@users.noreply.github.com>
2024-06-02 06:57:31 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountEmail = "Kumarsharvan0433@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState; // undefined variable 
+
+//accountId = 2 // not allowed 
+accountEmail = "hfd@.com"
+accountPassword = "21312241"
+accountCity = "Bengeluru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and funtional scope
+ */
